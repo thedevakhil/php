2fe5baeb58cae6aae3e820a44753bd658cb9430c
Akhil Shaji <98893138+thedevakhil@users.noreply.github.com>
2022-08-23 16:25:45 +0530

    Factorail Of Number

---
+++ a.php
@@ -0,0 +1,19 @@
+<html>
+<form method="POST" action="">Enter Number:
+<input type="text" name="number">
+<input type="submit" value="Calculate" name="ok">
+
+</form>
+</html>
+<?php
+
+$a=$_POST["number"];
+
+$fact=1;
+for($i=1;$i<$a;$i++){
+$fact=$fact+$fact*$i;
+
+}
+if(isset($_POST['ok']))//check if the button is pressed
+echo "Factorial=$fact";
+?>
