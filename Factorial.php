2b6433d89c80c24a3e3ac4ced7d7c2142b73f99b
Akhil Shaji <98893138+thedevakhil@users.noreply.github.com>
2022-08-23 18:56:23 +0530

    Factorial of a number

--- Factorial.php
+++ Factorial.php
@@ -2,18 +2,14 @@
 <form method="POST" action="">Enter Number:
 <input type="text" name="number">
 <input type="submit" value="Calculate" name="ok">
-
 </form>
 </html>
 <?php
-
 $a=$_POST["number"];
-
 $fact=1;
 for($i=1;$i<$a;$i++){
 $fact=$fact+$fact*$i;
-
 }
-if(isset($_POST['ok']))//check if the button is pressed
+if(isset($_POST['ok']))//check if the button is pressed not must
 echo "Factorial=$fact";
 ?>
